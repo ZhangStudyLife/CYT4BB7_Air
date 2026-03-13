3a0ed5773c32733afe5e788265a4139649723a88
ZhangStudyLife <2578464752@qq.com>
2026-03-13 22:22:00 +0800

    更稳定的速度环

--- project/code/FlightController/fc_params.c
+++ project/code/FlightController/fc_params.c
@@ -102,7 +102,7 @@ void FC_Params_Init(void)
     g_fc_params.pos_z_d_lpf = 0.20f;
 
     /* ===== X轴速度环参数 ===== */
-    g_fc_params.vel_x_kp = 0.12f;
+    g_fc_params.vel_x_kp = 0.2f;
     g_fc_params.vel_x_ki = 0.12f;
     g_fc_params.vel_x_kd = 0.0f;
     g_fc_params.vel_x_kff = 0.0f;
@@ -110,7 +110,7 @@ void FC_Params_Init(void)
     g_fc_params.vel_x_d_lpf = 0.0f;
 
     /* ===== Y轴速度环参数 ===== */
-    g_fc_params.vel_y_kp = 0.12f;
+    g_fc_params.vel_y_kp = 0.2f;
     g_fc_params.vel_y_ki = 0.12f;   
     g_fc_params.vel_y_kd = 0.0f;
     g_fc_params.vel_y_kff = 0.0f;

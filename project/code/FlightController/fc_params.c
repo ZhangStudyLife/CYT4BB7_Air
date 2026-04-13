c1a6cb79281b0a8c44e720f7bdf7ebc42fc4cedb
ZhangStudyLife <zyz051013@gmail.com>
2026-04-13 21:59:58 +0800

    这次可以正常飞行,但是手动模式打角过大,会导致飞行的tof融合值异常

--- project/code/FlightController/fc_params.c
+++ project/code/FlightController/fc_params.c
@@ -77,8 +77,8 @@ static void fc_params_fill_defaults(fc_params_t *params)
 
     /* ===== 油门与机械配平参数 ===== */
     params->base_throttle = 3150;         /* 悬停油门 */
-    params->roll_mech_trim_deg = -2.0f;   /* Roll 机械配平角 */
-    params->pitch_mech_trim_deg = 7.0f; /* Pitch 机械配平角 */
+    params->roll_mech_trim_deg = 2.95f;   /* Roll 机械配平角 */
+    params->pitch_mech_trim_deg = 1.75f; /* Pitch 机械配平角 */
 
     /* ===== Roll 轴角速度环参数 ===== */
     params->roll_gyro_kp = 2.5f;
@@ -153,16 +153,16 @@ static void fc_params_fill_defaults(fc_params_t *params)
     params->pos_z_d_lpf = 2.0f;
 
     /* ===== X 轴速度环参数 ===== */
-    params->vel_x_kp = 0.095f;
-    params->vel_x_ki = 0.024f;
+    params->vel_x_kp = 0.14f;
+    params->vel_x_ki = 0.020f;
     params->vel_x_kd = 0.0f;
     params->vel_x_kff = 0.0f;
     params->vel_x_i_limit = 4.5f;
     params->vel_x_d_lpf = 0.0f;
 
     /* ===== Y 轴速度环参数 ===== */
-    params->vel_y_kp = 0.095f;
-    params->vel_y_ki = 0.024f;
+    params->vel_y_kp = 0.14f;
+    params->vel_y_ki = 0.020f;
     params->vel_y_kd = 0.0f;
     params->vel_y_kff = 0.0f;
     params->vel_y_i_limit = 4.5f;

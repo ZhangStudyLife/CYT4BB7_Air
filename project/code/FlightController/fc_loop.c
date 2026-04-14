0a6ade749ed244fc3e93021b85eeeb136f09f893
ZhangStudyLife <zyz051013@gmail.com>
2026-04-14 20:39:31 +0800

    实际飞行了一次,根据新飞机重新调节角速度加速度的滤波参数

--- project/code/FlightController/fc_loop.c
+++ project/code/FlightController/fc_loop.c
@@ -478,25 +478,25 @@ void FC_Loop_100Hz(void)
         s_hover_throttle += alpha * (throttle_z_cmd - s_hover_throttle);
         s_hover_throttle = fc_clampf(s_hover_throttle, FC_HOVER_THR_MIN, FC_HOVER_THR_MAX);
     }
-    if (FC_START_CRSF_Get_State() == FC_START_CRSF_STATE_FLYING)
-    {
-        wifi_justfloat(tick_1000us_cnt,
-                       target_height_m * 1000.0f,
-                       g_tof_fused_height_mm,
-                       height_err_mm,
-                       height_pos_out,
-                       s_height_vz_mps,
-                       height_vz_raw_mps,
-                       height_vel_pid.error,
-                       height_vel_pid.p_term,
-                       height_vel_pid.i_term,
-                       height_vel_pid.d_term,
-                       height_vel_out,
-                       throttle_z_cmd,
-                       g_tof_measure_height_mm,
-                       g_tof_fused_valid,
-                       g_tof_measure_mask);
-    }
+    // if (FC_START_CRSF_Get_State() == FC_START_CRSF_STATE_FLYING)
+    // {
+    //     wifi_justfloat(tick_1000us_cnt,
+    //                    target_height_m * 1000.0f,
+    //                    g_tof_fused_height_mm,
+    //                    height_err_mm,
+    //                    height_pos_out,
+    //                    s_height_vz_mps,
+    //                    height_vz_raw_mps,
+    //                    height_vel_pid.error,
+    //                    height_vel_pid.p_term,
+    //                    height_vel_pid.i_term,
+    //                    height_vel_pid.d_term,
+    //                    height_vel_out,
+    //                    throttle_z_cmd,
+    //                    g_tof_measure_height_mm,
+    //                    g_tof_fused_valid,
+    //                    g_tof_measure_mask);
+    // }
 
     switch (s_flight_mode)
     {

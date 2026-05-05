8a6f7f5d636012891f382988d964bd1fc5cd162b
ZhangStudyLife <zyz051013@gmail.com>
2026-05-05 15:22:08 +0800

    可以在核1看到摄像头在114上面

--- project/user/main_cm7_1.c
+++ project/user/main_cm7_1.c
@@ -4,7 +4,25 @@ int main(void)
 {
     clock_init(SYSTEM_CLOCK_250M);
     debug_init();
+    ips114_init();
+    ips114_show_string(0, 0, "mt9v03x init.");
+
     image_init();
+    system_delay_ms(100);
+    while(0U == mt9v03x_finish_flag)
+    {
+        if(mt9v03x_init())
+        {
+            ips114_show_string(0, 16, "mt9v03x reinit.");
+            system_delay_ms(500);
+        }
+        else
+        {
+            break;
+        }
+    }
+    ips114_show_string(0, 16, "init success.");
+
     SCB_DisableDCache();
     ipc_communicate_init(IPC_PORT_2, ipc_image_callback);
 
@@ -12,6 +30,10 @@ int main(void)
 
     while(true)
     {
+        if(mt9v03x_finish_flag)
+        {
+            ips114_displayimage03x((const uint8 *)mt9v03x_image, MT9V03X_W, MT9V03X_H);
+        }
         image_update();
         ipc_image_send();
 

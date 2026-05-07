2a20e199e9e91629c11086e4ef7177ebb80e3cf0
ZhangStudyLife <zyz051013@gmail.com>
2026-05-07 23:45:42 +0800

    设置角度环前馈为0.10

--- project/code/FlightController/fc_params.c
+++ project/code/FlightController/fc_params.c
@@ -108,7 +108,7 @@ static void fc_params_fill_defaults(fc_params_t *params)
     params->roll_angle_kp = 6.0f;
     params->roll_angle_ki = 0.0f;
     params->roll_angle_kd = 0.0f;
-    params->roll_angle_kff = 0.03f;
+    params->roll_angle_kff = 0.10f;
     params->roll_angle_i_limit = 80.0f;
     params->roll_angle_d_lpf = 15.0f;
 
@@ -116,7 +116,7 @@ static void fc_params_fill_defaults(fc_params_t *params)
     params->pitch_angle_kp = 6.0f;
     params->pitch_angle_ki = 0.0f;
     params->pitch_angle_kd = 0.0f;
-    params->pitch_angle_kff = 0.03f;
+    params->pitch_angle_kff = 0.10f;
     params->pitch_angle_i_limit = 80.0f;
     params->pitch_angle_d_lpf = 15.0f;
 

c7016e8f30cc5781ed251a1422ec81f3ef7ca8ec
ZhangStudyLife <174326754+ZhangStudyLife@users.noreply.github.com>
2026-06-13 20:28:33 +0800

    核心1实现定时器实现更准的100hz

--- project/user/cm7_1_isr.c
+++ project/user/cm7_1_isr.c
@@ -38,6 +38,8 @@
 
 #include "zf_common_headfile.h"
 
+extern volatile uint8 g_camera_spi_tick_100hz;
+
 
 // **************************** PIT中断函数 ****************************
 void pit0_ch0_isr()                     // 定时器通道 0 周期中断服务函数      
@@ -63,6 +65,7 @@ void pit0_ch2_isr()                     // 定时器通道 2 周期中断服务函数
 void pit0_ch10_isr()                    // 定时器通道 10 周期中断服务函数      
 {
     pit_isr_flag_clear(PIT_CH10);
+    g_camera_spi_tick_100hz++;
 
 }
 
--- project/user/main_cm7_1.c
+++ project/user/main_cm7_1.c
@@ -1,15 +1,23 @@
 #include "zf_common_headfile.h"
 
+#define CAMERA_SPI_PIT       (PIT_CH10)
+
+volatile uint8 g_camera_spi_tick_100hz = 0U;
+
 int main(void)
 {
     clock_init(SYSTEM_CLOCK_250M);
     SCB_DisableDCache();
 
     CameraSpi_Init();
+    pit_ms_init(CAMERA_SPI_PIT, 10U);
 
     while(true)
     {
-        system_delay_ms(10U);
-        CameraSpi_Update();
+        if(g_camera_spi_tick_100hz > 0U)
+        {
+            g_camera_spi_tick_100hz--;
+            CameraSpi_Update();
+        }
     }
 }
